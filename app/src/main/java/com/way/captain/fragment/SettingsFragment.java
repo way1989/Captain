f99c2b8687eed4ed926666e3ff0fde14762a7cf0
li.weiping <li.weiping@tinno.com>
2016-03-26 16:13:46 +0800

    add new feature

--- app/src/main/java/com/way/captain/fragment/SettingsFragment.java
+++ app/src/main/java/com/way/captain/fragment/SettingsFragment.java
@@ -2,15 +2,17 @@ package com.way.captain.fragment;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
-import android.text.TextUtils;
 
 import com.way.captain.R;
 import com.way.captain.service.ChatHeadService;
@@ -19,9 +21,10 @@ import com.way.captain.utils.AppUtils;
 /**
  * Created by android on 16-2-4.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String HIDE_FLOATVIEW_KEY = "key_hide_floatview";
     public static final String VIDEO_SIZE_KEY = "key_video_size_percentage";
+    public static final String VIDEO_STOP_METHOD_KEY = "key_video_stop_method";
     public static final String SHOW_COUNTDOWN_KEY = "key_three_second_countdown";
     public static final String SHOW_TOUCHES_KEY = "key_show_touches";
     public static final String ATOUCH_KEY = "key_use_atouch";
@@ -43,22 +46,42 @@ public class SettingsFragment extends PreferenceFragment {
         } catch (final PackageManager.NameNotFoundException e) {
             findPreference(VERSION_KEY).setSummary("?");
         }
+        ListPreference sizeListPreference = (ListPreference) findPreference(VIDEO_SIZE_KEY);
+        sizeListPreference.setSummary(sizeListPreference.getEntry());
+        ListPreference stopMethodListPreference = (ListPreference) findPreference(VIDEO_STOP_METHOD_KEY);
+        stopMethodListPreference.setSummary(stopMethodListPreference.getEntry());
         if (AppUtils.isMarshmallow()) {
             PreferenceCategory preferenceScreen = (PreferenceCategory) findPreference("key_advance_category");
             preferenceScreen.removePreference(findPreference(SHOW_TOUCHES_KEY));
         }
+        PreferenceManager.getDefaultSharedPreferences(getActivity()).registerOnSharedPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
         String key = preference.getKey();
-        if (TextUtils.equals(key, ATOUCH_KEY)) {
-            SwitchPreference switchPreference = (SwitchPreference) preference;
-            if (switchPreference.isChecked())
-                mContext.startService(new Intent(mContext, ChatHeadService.class));
-            else
-                mContext.stopService(new Intent(mContext, ChatHeadService.class));
+        switch (key) {
+            case ATOUCH_KEY:
+                SwitchPreference switchPreference = (SwitchPreference) preference;
+                if (switchPreference.isChecked())
+                    mContext.startService(new Intent(mContext, ChatHeadService.class));
+                else
+                    mContext.stopService(new Intent(mContext, ChatHeadService.class));
+                break;
+
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        switch (key) {
+            case VIDEO_SIZE_KEY:
+            case VIDEO_STOP_METHOD_KEY:
+                ListPreference listPreference = (ListPreference) findPreference(key);
+                listPreference.setSummary(listPreference.getEntry());
+                break;
+        }
     }
 }
