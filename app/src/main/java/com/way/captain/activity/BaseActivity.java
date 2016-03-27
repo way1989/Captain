a2a0d757aa9d93e143a81e647aadbfdf3d91d499
way <1320438999@qq.com>
2016-03-27 14:16:56 +0800

    fix bug

--- app/src/main/java/com/way/captain/activity/BaseActivity.java
+++ app/src/main/java/com/way/captain/activity/BaseActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 
+import com.way.captain.BuildConfig;
 import com.way.firupgrade.FIRUtils;
 
 //import com.bugtags.library.Bugtags;
@@ -16,27 +17,28 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FIRUtils.checkForUpdate(this, false);
+        if (BuildConfig.FIR_ENABLED)
+            FIRUtils.checkForUpdate(this, false);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         //注：回调 1
-  //      Bugtags.onResume(this);
+        //      Bugtags.onResume(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         //注：回调 2
-    //    Bugtags.onPause(this);
+        //    Bugtags.onPause(this);
     }
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         //注：回调 3
-      //  Bugtags.onDispatchTouchEvent(this, event);
+        //  Bugtags.onDispatchTouchEvent(this, event);
         return super.dispatchTouchEvent(event);
     }
 }
